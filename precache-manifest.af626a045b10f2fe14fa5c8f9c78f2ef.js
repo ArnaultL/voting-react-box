fba24be691bab1522d515f14f82d6fdcdc84869b
Arnault <arnault.lalloz@gmail.com>
2022-06-07 00:43:45 +0200

    Updates

---
+++ precache-manifest.af626a045b10f2fe14fa5c8f9c78f2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325bf0469bd395dc57a2a154d45ebffc",
+    "url": "/voting-react-box/index.html"
+  },
+  {
+    "revision": "16db2434790701b2f01b",
+    "url": "/voting-react-box/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "869a2dceb33a9df14e98",
+    "url": "/voting-react-box/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "16db2434790701b2f01b",
+    "url": "/voting-react-box/static/js/2.4320d6ee.chunk.js"
+  },
+  {
+    "revision": "869a2dceb33a9df14e98",
+    "url": "/voting-react-box/static/js/main.94949c2f.chunk.js"
+  },
+  {
+    "revision": "318de969c732e1c697e4",
+    "url": "/voting-react-box/static/js/runtime-main.7e9fcbc1.js"
+  }
+]);
